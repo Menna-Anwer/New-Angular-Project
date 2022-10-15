5d36831f94ac2c02bc16e02cae5990b989e8699c
Menna-Anwer <mennaanwer588@gmail.com>
2022-10-15 23:44:09 +0200

    my work

--- src/app/Components/all-users/all-users.component.ts
+++ src/app/Components/all-users/all-users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl , FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { IUser } from 'src/app/interfaces/iuser';
+
 
 @Component({
   selector: 'app-all-users',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-users.component.css']
 })
 export class AllUsersComponent implements OnInit {
-
-  constructor() { }
+  addUserForm: FormGroup;
+  constructor() {
+    this.addUserForm=new FormGroup({
+      name: new FormControl('', [Validators.required,Validators.pattern("[A-Za-z]{3,}")]),
+      email:new FormControl("",[Validators.required,Validators.pattern("[^ @]*@[^ @]*")]),
+      image:new FormControl(""),
+      password: new FormControl('',[
+        Validators.minLength(8),
+        Validators.required]),
+      repeatPassword: new FormControl('')
+    },{validators:this.samePassword})
+   }
 
   ngOnInit(): void {
   }
+  addUser(){
+     return this.addUserForm.value as IUser
+  }
+  samePassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null =>{
+    const pass = control.get('password');
+    const rePass = control.get('repeatPassword');
+   
+    return pass && rePass && pass.value !== rePass.value ? {notSame: true} : null
+  }
+
 
 }
--- src/app/interfaces/iuser.ts
+++ src/app/interfaces/iuser.ts
@@ -2,6 +2,5 @@ export interface IUser {
     name:string,
     email:string,
     password:string,
-    roomNo:number,
-    image:string
+    image?:string
 }
